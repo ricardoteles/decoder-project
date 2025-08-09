b4b45996e0b4e9458895ba01b9cb0af0aea7fafc
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-09 11:46:50 -0300

    API RESTful com Spring Web

---
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId")UUID userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(userId).get());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId")UUID userId) {
+        userService.delete(userService.findById(userId).get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfuly.");
+    }
+}
