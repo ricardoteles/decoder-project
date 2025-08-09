7befb0192d6fb45417a939d45a4f7212402d11f2
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-09 15:03:16 -0300

    JsonView - Multiplas Visualizações em APIs

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,9 @@ public class AuthenticationController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserRecordDto userRecordDto) {
+    public ResponseEntity<Object> registerUser(@RequestBody
+                                               @JsonView(UserRecordDto.UserView.RegistrationPost.class)
+                                               UserRecordDto userRecordDto) {
         if(userService.existsByUsername(userRecordDto.username())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is already taken!");
         }
--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,13 +24,45 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId")UUID userId) {
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findById(userId));
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId")UUID userId) {
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
         userService.delete(userService.findById(userId));
         return ResponseEntity.status(HttpStatus.OK).body("User deleted successfuly.");
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
+                                             @RequestBody @JsonView(UserRecordDto.UserView.UserPut.class)
+                                             UserRecordDto userRecordDto) {
+        var userModel = userService.findById(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(userRecordDto, userModel));
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<Object> updatePassword(@PathVariable(value = "userId") UUID userId,
+                                             @RequestBody @JsonView(UserRecordDto.UserView.PasswordPut.class)
+                                             UserRecordDto userRecordDto) {
+        var userModel = userService.findById(userId);
+
+        if(!userModel.getPassword().equals(userRecordDto.oldPassword())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Mismatched old password!");
+        }
+
+        userService.updatePassword(userRecordDto, userModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Password updated successfully.");
+    }
+
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
+                                                 @RequestBody @JsonView(UserRecordDto.UserView.ImagePut.class)
+                                                 UserRecordDto userRecordDto) {
+        var userModel = userService.findById(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateImage(userRecordDto, userModel));
     }
 }
