7cbf46fa55520edde8ca765fed53e4163c701b61
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-10 15:59:18 -0300

    Configuração Global de CORS

--- authuser/src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,5 +16,13 @@ public class ResolverConfig implements WebMvcConfigurer {
         var pageableResolver = new PageableHandlerMethodArgumentResolver();
         pageableResolver.setFallbackPageable(PageRequest.of(0, 2));
         argumentResolvers.add(pageableResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//      An example of using CORS
+//      registry.addMapping("/users/**").allowedOrigins("http://example.com");
+//      registry.addMapping("/auth/**").allowedOrigins("http://abc.com");
+        registry.addMapping("/**").maxAge(3600);
     }
 }
