f22bc21664b03942e887f74ad492d7fb3a6a2ffc
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-13 11:21:00 -0300

    Principais Bibliotecas de Logging, Log Levels e Logging no Spring com Logback

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -4,18 +4,19 @@ import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+
     private final UserService userService;
 
     @PostMapping("/signup")
@@ -30,5 +31,16 @@ public class AuthenticationController {
         }
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDto));
+    }
+
+    @GetMapping("/logs")
+    public String index(){
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARN");
+        logger.error("ERROR");
+
+        return "Logging Spring Boot...";
     }
 }
