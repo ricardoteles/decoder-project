817d90585bf1e8bc4f926c65632e07d1a9777208
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-18 15:56:08 -0300

    API Composition Pattern - Preparar Métodos GetAll

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -27,8 +27,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec, Pageable pageable) {
-        Page<UserModel> userModelPage = userService.findAll(spec, pageable);
+    public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec,
+                                                       Pageable pageable,
+                                                       @RequestParam(required = false) UUID courseId) {
+        Page<UserModel> userModelPage = (courseId != null)
+                ? userService.findAll(SpecificationTemplate.userCourseId(courseId).and(spec), pageable)
+                : userService.findAll(spec, pageable);
 
         if(!userModelPage.isEmpty()) {
             for(UserModel user : userModelPage.toList()) {
--- authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -1,12 +1,16 @@
 package com.ead.authuser.specifications;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
+import jakarta.persistence.criteria.Join;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.UUID;
 
 public class SpecificationTemplate {
     @And({
@@ -17,4 +21,12 @@ public class SpecificationTemplate {
             @Spec(path = "fullName", spec = LikeIgnoreCase.class),
     })
     public interface UserSpec extends Specification<UserModel> {}
+
+    public static Specification<UserModel> userCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Join<UserModel, UserCourseModel> userJoin = root.join("usersCourses");
+            return cb.equal(userJoin.get("courseId"), courseId);
+        };
+    }
 }
