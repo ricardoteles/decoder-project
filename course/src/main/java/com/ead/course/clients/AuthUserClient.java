a4b2e6f54b1efcff110f2f491e186d9d604df098
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-20 14:54:59 -0300

    API Composition Pattern em Course Microservice

---
+++ course/src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -0,0 +1,45 @@
+package com.ead.course.clients;
+
+import com.ead.course.dtos.ResponsePageDto;
+import com.ead.course.dtos.UserRecordDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+
+import java.util.UUID;
+
+@Log4j2
+@Component
+public class AuthUserClient {
+    @Value("${ead.api.url.authuser}")
+    String baseUrlAuthuser;
+
+    private final RestClient restClient;
+
+    public AuthUserClient(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+    public Page<UserRecordDto> getAllUsersByCourse(UUID courseId, Pageable pageable){
+        String url = baseUrlAuthuser + "/users?courseId=" + courseId + "&page=" + pageable.getPageNumber() + "&size="
+                + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+
+        log.debug("Request URL: {}", url);
+
+        try {
+            return restClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<ResponsePageDto<UserRecordDto>>() {});
+
+        } catch(RestClientException e){
+            log.error("Error Request RestClient with cause: {}", e.getMessage());
+            throw new RuntimeException("Error Request RestClient", e);
+        }
+    }
+}
