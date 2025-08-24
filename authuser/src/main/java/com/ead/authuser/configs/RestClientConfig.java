cf82834eadf435445168cc7a5ae6f496a5687e83
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-24 14:07:26 -0300

    Merge 8-cross-cutting-registry-discovery-pattern into main

--- authuser/src/main/java/com/ead/authuser/configs/RestClientConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/RestClientConfig.java
@@ -1,11 +1,13 @@
 package com.ead.authuser.configs;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+    @LoadBalanced
     @Bean
     public RestClient.Builder restClientBuilder(){
         return RestClient.builder();
