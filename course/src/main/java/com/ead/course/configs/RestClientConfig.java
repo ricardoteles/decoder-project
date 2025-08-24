f62603cb77ebe7e0d22ec45d2956a94ed6e56903
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-24 14:06:12 -0300

    Comunicações Síncronas com Client-Side Discovery Pattern

--- course/src/main/java/com/ead/course/configs/RestClientConfig.java
+++ course/src/main/java/com/ead/course/configs/RestClientConfig.java
@@ -1,11 +1,13 @@
 package com.ead.course.configs;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+    @LoadBalanced
     @Bean
     public RestClient.Builder restClientBuilder(){
         return RestClient.builder();
