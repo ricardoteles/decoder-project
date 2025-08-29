f6a1ecd5119494c869c2246cb6bc79f5afdc27e2
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2025-08-29 11:09:17 -0300

    Criação Consumer e Recebimento Event State Transfer no Course Microservice

---
+++ course/src/main/java/com/ead/course/consumers/UserConsumer.java
@@ -0,0 +1,32 @@
+package com.ead.course.consumers;
+
+import com.ead.course.dtos.UserEventRecordDto;
+import com.ead.course.enums.ActionType;
+import com.ead.course.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserConsumer {
+    private final UserService userService;
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
+            exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}",
+            type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
+    )
+    public void listenUserEvent(@Payload UserEventRecordDto userEventRecordDto) {
+        var userModel = userEventRecordDto.convertToUserModel();
+
+        switch (ActionType.valueOf(userEventRecordDto.actionType())) {
+            case CREATE -> userService.save(userModel);
+        }
+    }
+}
---
+++ course/src/main/java/com/ead/course/dtos/UserEventRecordDto.java
@@ -0,0 +1,25 @@
+package com.ead.course.dtos;
+
+import com.ead.course.models.UserModel;
+import org.springframework.beans.BeanUtils;
+
+import java.util.UUID;
+
+public record UserEventRecordDto(
+        UUID userId,
+        String username,
+        String email,
+        String fullName,
+        String userStatus,
+        String userType,
+        String phoneNumber,
+        String imageUrl,
+        String actionType
+) {
+    public UserModel convertToUserModel() {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(this, userModel);
+
+        return userModel;
+    }
+}
